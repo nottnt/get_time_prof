5fb50cc7d14f9781511b6d845a0f4881b8900652
nottnt <tanatat19@outlook.com>
2018-04-24 00:09:00 +0700

    test nm.js

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show:false });
+var express = require('express')
+var app = express()
 
-const URL = 'http://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/';
-console.log('Welcome to Nightmare scrape\n==========');
 
+app.get('/', function (req, res) {
 nightmare
-    .goto(URL)
-    .wait('.post-title')
-    .evaluate(() => document.querySelector('.post-title').textContent)
-    .end()
-    .then((result) => {
-        console.log(result);
-        console.log('=========\nAll done');
-    })
-    .catch((error) => {
-        console.error('an error has occurred: ' + error);
-    })
-    .then(() => (console.log('process exit'), process.exit()));
+  .goto('http://klogic.kmutnb.ac.th:8080/kris/tess/dataQuerySelector.jsp?query=teachTab')
+  .wait(1000)
+  .select('select[name="facCode"]', '06')
+  .wait(1000)
+  .select('select[name="lectCode"]', 'AMK')
+  .wait(1000)
+  .evaluate(function () {
+    var data = []
+    var x = document.body.getElementsByTagName('table')[6].getElementsByTagName('tr')
+    for (var i = 1; i < x.length; i++) {
+      data.push(
+        {
+          id: x[i].getElementsByTagName('td')[0].innerText,
+          name: x[i].getElementsByTagName('td')[1].innerText
+        }
+      )
+    }
+    return data
+  })
+  .end()
+  .then(value => {
+  
+        res.send({value})
+     
+
+  })
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
+})
