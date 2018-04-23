1070b18aca55d4c787558156246eb320aee8efec
nottnt <tanatat19@outlook.com>
2018-04-23 16:06:42 +0700

    ppp

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: false });
+const nightmare = Nightmare({ electronPath: require('electron') });
 
 const URL = 'http://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/';
 console.log('Welcome to Nightmare scrape\n==========');
