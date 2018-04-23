146e0fb663ff53e65a40cddb5468926b541febba
nottnt <tanatat19@outlook.com>
2018-04-23 14:22:39 +0700

    pray

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
+var express = require('express')
+var app = express()
 
+app.set('port',(process.env.PORT || 9000))
 const URL = 'http://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/';
 console.log('Welcome to Nightmare scrape\n==========');
 
