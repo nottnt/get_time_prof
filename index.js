9d05fc72ca6bab4d64f6f595ef00519a86784f2b
nottnt <tanatat19@outlook.com>
2018-04-30 18:53:26 +0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show:false });
+const nightmare = Nightmare({ show:true });
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 5000;
@@ -8,15 +8,19 @@ app.listen(port, function() {
     console.log('Starting node.js on port ' + port)
   })
 
-app.get('/gettime', function (req, res) {
+app.get('/gettime', function gg (req, res) {
+  console.log('go to the hell')
+
+
 nightmare
+  
   .goto('http://klogic.kmutnb.ac.th:8080/kris/tess/dataQuerySelector.jsp?query=teachTab')
   .wait(1000)
   .select('select[name="facCode"]', '06')
   .wait(1000)
   .select('select[name="lectCode"]', 'AMK')
   .wait(1000)
-  .evaluate(function () {
+  .evaluate(function gg() {
     var data = []
     var x = document.body.getElementsByTagName('table')[6].getElementsByTagName('tr')
     for (var i = 1; i < x.length; i++) {
@@ -27,16 +31,20 @@ nightmare
         }
       )
     }
+    
     return data
   })
+  
   .end()
   .then(value => {
-  
-        res.send({value})
+     res.send(value)
      
-
+    process.exit(0);
+    
   })
+  
   .catch(error => {
     console.error('Search failed:', error)
   })
+  
 })
